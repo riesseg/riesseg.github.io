40df92f67818de940ca6f9c7322f33dd12ee21af
GollGotth <riesseg@gmail.com>
2023-12-08 11:19:22 +0100

    refactoring check if bingo

--- functions/grille.js
+++ functions/grille.js
@@ -1,3 +1,20 @@
+var bingoLines = [
+  [0,1,2,3,4],
+  [5,6,7,8,9],
+  [10,11,12,13,14],
+  [15,16,17,18,19],
+  [20,21,22,23,24],
+
+  [0,5,10,15,20],
+  [1,6,11,16,21],
+  [2,7,12,17,22],
+  [3,8,13,18,23],
+  [4,9,14,19,24],
+
+  [0,6,12,18,24],
+  [4,8,12,16,20]
+];
+
 export var listGrids  = [
   ["Secret d'histoire", "secret_histoire.txt"],
   ["Ravioli", "ravioli.txt"]
@@ -12,3 +29,16 @@ export function loadGrid() {
     });
 }
 
+export function isBingo() {
+  var bool = false;
+  $.each( bingoLines, function( key, line ) {
+      if(line.every(isSelected)) {
+        line.forEach((item, index, arr) => $("#c"+ item).addClass('bingo'));
+        bool = true;
+      }  
+   });
+   return bool;  
+}; 
+function isSelected(index) {
+    return $("#c"+ index).hasClass("selected")
+}
--- functions/main.js
+++ functions/main.js
@@ -1,11 +1,8 @@
 import { setGrid, resetGrid } from "./function.js";
-import { loadGrid, listGrids  } from "./grille.js";
+import { loadGrid, listGrids, isBingo  } from "./grille.js";
 import { itsBingo, continueBingo, resetBingoAnim } from "./rain.js";
 
-var grille = new Array();
-var grille2 = new Array();
 var selectedGrid = listGrids[0][1];
-const fichierLoaded = 0;
 var displayInterval;
 var transparencyTrigger = false;
 
@@ -23,19 +20,6 @@ export function hello() {
 
     transparencyTrigger = false;
 });
-
-function isSelected(index) {
-    return $("#c"+ index).hasClass("selected")
-}
-
-function checkLine(indexesToCheck) {
-    if(indexesToCheck.every(isSelected)) {
-        indexesToCheck.forEach((item, index, arr) => $("#c"+ item).addClass('bingo'))
-        itsBingo();
-    }
-}
-
-
 
 function setTransparencyMode() {
     transparencyTrigger = !transparencyTrigger;
@@ -75,20 +59,9 @@ function startDisplayInterval() {
 
 $(".cell").on('click', function(){
     $(this).toggleClass('selected');
-    checkLine([0,1,2,3,4]);
-    checkLine([5,6,7,8,9]);
-    checkLine([10,11,12,13,14]);
-    checkLine([15,16,17,18,19]);
-    checkLine([20,21,22,23,24]);
-
-    checkLine([0,5,10,15,20]);
-    checkLine([1,6,11,16,21]);
-    checkLine([2,7,12,17,22]);
-    checkLine([3,8,13,18,23]);
-    checkLine([4,9,14,19,24]);
-
-    checkLine([0,6,12,18,24]);
-    checkLine([4,8,12,16,20]);
+    if (isBingo()){
+        itsBingo();
+    }
 });
 
 $("#selectGrid").on('change', function () {
